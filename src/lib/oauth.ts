1c2d213e30a6ee70f953962064d27627bd293b6e
olliveer <j.oliveira90@live.com>
2025-06-20 14:08:46 -0300

    fix: oauth redirect url

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,16 +1,17 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { headers } from 'next/headers';
 import { OAuthProvider } from 'node-appwrite';
 import { createAdminClient } from './appwrite';
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  const origin = (await headers()).get('origin');
-
-  const redirectUrl = await account.createOAuth2Token(OAuthProvider.Github, `${origin}/oauth`, `${origin}/sign-up`);
+  const redirectUrl = await account.createOAuth2Token(
+    OAuthProvider.Github,
+    `${process.env.NEXT_PUBLIC_API_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_API_URL}/sign-up`,
+  );
 
   return redirect(redirectUrl);
 }
