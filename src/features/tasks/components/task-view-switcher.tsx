daaaf95cc39a984fcf2f7900cf149d81ca8d7a9a
olliveer <j.oliveira90@live.com>
2025-06-20 13:18:22 -0300

    refactor: dynamic page description

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -18,6 +18,7 @@ import { DataKanban } from './data-kanban';
 import { TaskStatus } from '../types';
 import { useBulkUpdateTasks } from '../api/use-bulk-update-tasks';
 import { DataCalendar } from './data-calendar';
+import { useProjectId } from '@/features/projects/hooks/use-project-id';
 
 interface TaskViewSwitcherProps {
   hideProjectFilter?: boolean;
@@ -29,10 +30,11 @@ export function TaskViewSwitcher({ hideProjectFilter }: TaskViewSwitcherProps) {
     defaultValue: 'table',
   });
   const workspaceId = useWorkspaceId();
+  const paramProjectId = useProjectId();
   const { open } = useCreateTaskModal();
   const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
     workspaceId,
-    projectId,
+    projectId: paramProjectId || projectId,
     status,
     assigneeId,
     search,
